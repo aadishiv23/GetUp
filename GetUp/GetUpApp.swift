3f162530c27331ea635bbe0154bf2ba722c062a5
aadishiv <aadishiv@outlook.com>
2024-03-24 21:39:49 -0400

    start

---
+++ GetUp/GetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpApp.swift
+//  GetUp
+//
+//  Created by Aadi Shiv Malhotra on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
